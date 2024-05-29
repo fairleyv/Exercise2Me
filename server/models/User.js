f0b6bbddf83ae2d125a6679ff03a447a5caba58d
fairleyv <fairleyvincent@gmail.com>
2024-05-28 17:09:47 -0700

    corrected error to models favorite and saved exercises and changed exerciseSchema variable name

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// import schema from Exercise.js
+// import model from Exercise.js
 const Exercise = require('./Exercise');
 
 const userSchema = new Schema(
