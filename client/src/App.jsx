eda44340d08acf335398ba7d6249812c3c1bd372
Chaosgale <yantus@gmail.com>
2024-05-30 20:30:46 -0500

    mantine components and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+import Demo from './components/mantine/Grid';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    
+      <MantineProvider>
+        <>
+        <Navbar />
+        <Outlet />
+
+        <Demo />
+        </>
+      </MantineProvider>
   );
 }
 
