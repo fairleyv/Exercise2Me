137089687bb5eae0c136b74167478b6a61e41c8b
Chaosgale <yantus@gmail.com>
2024-06-03 21:51:02 -0500

    added paper

--- client/src/components/mantine/ExerciseList.jsx
+++ client/src/components/mantine/ExerciseList.jsx
@@ -1,1 +1,13 @@
+import { Title, Paper } from '@mantine/core';
 
+export function ExerciseList() {
+    return (
+        <>
+      <Paper shadow="lg" p="xl">
+        <h3>
+         Biceps Curls
+        </h3>
+      </Paper>
+      </>
+    );
+  }
