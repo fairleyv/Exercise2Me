215fe3adca6803a9d533a507b9c3f818ac6fcb03
unknown <youyu00022@gmail.com>
2024-06-03 19:43:17 -0500

    added in the Drag and Droppables

--- client/src/components/mantine/Grid.jsx
+++ client/src/components/mantine/Grid.jsx
@@ -2,13 +2,15 @@ import { Grid, Container } from '@mantine/core';
 
 import { ExerciseCard } from './ExerciseCard';
 
+import { ListExercise } from './ListExercise'
+
 
 export default function Demo() {
   return (
     <>
     <Grid justify="space-around">
       <Grid.Col span={{ base: 12, md: 5}}>
-        <Container fluid> Hello </Container>
+        <Container fluid> <ListExercise></ListExercise> </Container>
         </Grid.Col>
 
       <Grid.Col span={{ base: 12, md: 5}}>
