019daca9fe0515b6c0fd621c367337fda60141b2
Onyx Vega <mabs.24gl@gmail.com>
2024-06-09 19:20:41 -0400

    getting all the data instead of the clicked on data

--- client/src/components/mantine/ExerciseCard.jsx
+++ client/src/components/mantine/ExerciseCard.jsx
@@ -4,79 +4,152 @@ import classes from './ExerciseCard.module.css';
 import { ExerciseContext } from '../../context/exerciseContext';
 import { useEffect, useState, useContext } from 'react';
 
-const mockdata = {
-  image:
-    'https://steelsupplements.com/cdn/shop/articles/shutterstock_657941434_376ae0c9-1a39-42d3-bc39-3eaf18d5038f_1000x.jpg?v=1641548776',
-  title: 'Biceps curl',
-  country: 'Upper Body',
-  description:
-    'Stand with feet shoulder-width apart, holding dumbbells with arms fully extended and palms facing forward. Keep elbows close to your torso. Curl the weights by contracting your biceps until the dumbbells reach shoulder level, exhaling as you lift. Pause and squeeze your biceps at the top, then slowly lower the dumbbells back while inhaling. Repeat for desired reps. Avoid swinging your body or using momentum.',
-//   badges: [
-//     { emoji: '☀️', label: 'Sunny weather' },
-//     { emoji: '🦓', label: 'Onsite zoo' },
-//     { emoji: '🌊', label: 'Sea' },
-//     { emoji: '🌲', label: 'Nature' },
-//     { emoji: '🤽', label: 'Water sports' },
-//   ],
-};
-
+// have selectedExercise onClick
 export function ExerciseCard() {
-  const { image, title, description, country, badges } = mockdata;
 
-  return (
-    <Card withBorder radius="md" p="md" className={classes.card}>
+  const { selectedExerciseFormatted } = useContext(ExerciseContext);
+  // map data
+
+  const selected = selectedExerciseFormatted.map((exercise) => (
+    <>
       <Card.Section>
-        <Image src={image} alt={title} height={180} />
+        <Image src={exercise.image} alt={exercise.exerciseName} height={180} />
       </Card.Section>
 
       <Card.Section className={classes.section} mt="md">
         <Group justify="apart">
           <Text fz="lg" fw={500}>
-            {title}
+            {exercise.exerciseName}
           </Text>
-          <Badge size="sm" variant="light" color= "purple" >
-            {country}
+          <Badge size="sm" variant="light" color="purple" >
+            {exercise.groupName}
           </Badge>
         </Group>
         <Text fz="sm" mt="xs">
-          {description}
+          {exercise.description}
         </Text>
       </Card.Section>
 
-
-
-      {/* <Card.Section className={classes.section}>
-        <Text mt="md" className={classes.label} c="dimmed">
-          Perfect for you, if you enjoy
-        </Text>
-        <Group gap={7} mt={5}>
-          {features}
-        </Group>
-      </Card.Section> */}
-
-       
-    <Card.Section className={classes.section}>
+      <Card.Section className={classes.section}>
         <div className={classes.iconContainer}>
-            <div>
-                <IconBarbell className={classes.like} stroke={1.5} />
-                <h6>Dumbbells</h6>
-            </div>
-            <div>
-                <IconChartBarPopular className={classes.like} stroke={1.5} />
-                <h6>Easy</h6>
-            </div>
+          <div>
+            <IconBarbell className={classes.like} stroke={1.5} />
+            <h6>{exercise.equipmentNeeded}</h6>
+          </div>
+          <div>
+            <IconChartBarPopular className={classes.like} stroke={1.5} />
+            <h6>{exercise.difficulty}</h6>
+          </div>
         </div>
-     </Card.Section>
-       
+      </Card.Section>
 
       <Group mt="xs">
-        <Button radius="md" style={{ flex: 1 }} color= "#198754">
+        <Button radius="md" style={{ flex: 1 }} color="#198754">
           Save to My Routine
         </Button>
         {/* <ActionIcon variant="default" radius="md" size={36}>
           <IconBarbell className={classes.like} stroke={1.5} />
-        </ActionIcon> */}
+          </ActionIcon> */}
       </Group>
-    </Card>
+    </>
+  ))
+
+  return (
+    <>
+      <Card withBorder radius="md" p="md" className={classes.card}>
+        {selected}
+      </Card>
+    </>
   );
 }
+
+// const { selectedExerciseFormatted } = useContext(ExerciseContext);
+
+// const [selectedExercise, setSelectedExercise] = useState([]);
+
+// useEffect(() => {
+//   setSelectedExercise(selectedExerciseFormatted)
+// }, [selectedExerciseFormatted]);
+
+//   return (
+//     <>
+//       {selectedExercise ? (
+//         <Card withBorder radius="md" p="md" className={classes.card}>
+//           <Card.Section>
+//             <Image src={selectedExercise.image} alt={selectedExercise.exerciseName} height={180} />
+//           </Card.Section>
+
+//           <Card.Section className={classes.section} mt="md">
+//             <Group justify="apart">
+//               <Text fz="lg" fw={500}>
+//                 {selectedExercise.exerciseName}
+//               </Text>
+//               <Badge size="sm" variant="light" color="purple">
+//                 {/* {selectedExercise[1].groupName} */}
+//               </Badge>
+//             </Group>
+//             <Text fz="sm" mt="xs">
+//               {selectedExercise.description}
+//             </Text>
+//           </Card.Section>
+
+//           <Card.Section className={classes.section}>
+//             <div className={classes.iconContainer}>
+//               <div>
+//                 <IconBarbell className={classes.like} stroke={1.5} />
+//                 <h6>{selectedExercise.equipmentNeeded}</h6>
+//               </div>
+//               <div>
+//                 <IconChartBarPopular className={classes.like} stroke={1.5} />
+//                 <h6>{selectedExercise.difficulty}</h6>
+//               </div>
+//             </div>
+//           </Card.Section>
+
+//           <Group mt="xs">
+//             <Button radius="md" style={{ flex: 1 }} color="#198754">
+//               Save to My Routine
+//             </Button>
+//           </Group>
+//         </Card>
+//       ) : (
+//         <div>
+//           {selectedExerciseFormatted.map((exercise, index) => (
+//             <Card withBorder radius="md" p="md" className={classes.card} key={index} onClick={handleOnClick}>
+//               <Card.Section>
+//                 <Image src={exercise.image} alt={exercise.exerciseName} height={180} />
+//               </Card.Section>
+
+//               <Card.Section className={classes.section} mt="md">
+//                 <Group justify="apart">
+//                   <Text fz="lg" fw={500}>
+//                     {exercise.exerciseName}
+//                   </Text>
+//                   <Badge size="sm" variant="light" color="purple">
+//                     {exercise.group.groupName}
+//                   </Badge>
+//                 </Group>
+//                 <Text fz="sm" mt="xs">
+//                   {exercise.description}
+//                 </Text>
+//               </Card.Section>
+
+//               <Card.Section className={classes.section}>
+//                 <div className={classes.iconContainer}>
+//                   <div>
+//                     <IconBarbell className={classes.like} stroke={1.5} />
+//                     <h6>{exercise.equipmentNeeded}</h6>
+//                   </div>
+//                   <div>
+//                     <IconChartBarPopular className={classes.like} stroke={1.5} />
+//                     <h6>{exercise.difficulty}</h6>
+//                   </div>
+//                 </div>
+//               </Card.Section>
+//             </Card>
+//           ))}
+//         </div>
+//       )}
+//     </>
+//   );
+// }
--- client/src/components/mantine/ExerciseList.jsx
+++ client/src/components/mantine/ExerciseList.jsx
@@ -3,22 +3,28 @@ import { ExerciseContext } from '../../context/exerciseContext';
 import { useEffect, useState, useContext } from 'react';
 
 export function ExerciseList() {
-  const { groupSearchFormatted } = useContext(ExerciseContext);
+  const { groupSearchFormatted, exerciseChoice, selectedExerciseFormatted } = useContext(ExerciseContext);
+  // const { setSelectedExercise } = useContext(ExerciseContext);
 
   const [exercises, setExercises] = useState([]);
   useEffect(() => {
     setExercises(groupSearchFormatted)
-    console.log(groupSearchFormatted);
-    console.log(exercises);
+    // console.log(groupSearchFormatted);
+    // console.log(exercises);
   }, [groupSearchFormatted])
   // get group name to appear
   const groupName = exercises.length > 0 ? exercises[1].groupName : '';
-  console.log(groupName);
 
-  const featured = exercises.map((exercise) => (
+  const handleExerciseClick = (exercise) => {
+    exerciseChoice(exercise.exerciseName);
+  };
+
+  // handle onClick event for selected exercise
+  const featured = groupSearchFormatted.map((exercise) => (
     <>
       <Divider my="md" />
-      <UnstyledButton>
+      <UnstyledButton onClick={() => handleExerciseClick(exercise)}>
+      {/* <UnstyledButton> */}
         {exercise.exerciseName}
       </UnstyledButton>
     </>
